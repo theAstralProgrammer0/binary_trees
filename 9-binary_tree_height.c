9cee7f10520abf1c27efd661cbde5bc341458b0a
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-03-26 19:50:07 +0100

    Hopefully should work

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,23 +11,24 @@
  *
  * Return: Nothing
  */
-size_t height_checker(const binary_tree_t *node, size_t *height)
+void height_checker_left(const binary_tree_t *node, size_t *h)
 {
 	if (!node)
-		return (*height);
+		return;
 
-	if (node->left)
-	{
-		(*height)++;
-		height_checker(node->left, height);
-	}
-	else if (node->right)
-	{
-		(*height)++;
-		height_checker(node->right, height);
-	}
-	return (*height);
+	(*h)++;
+	height_checker_left(node->left, h);
 }
+
+void height_checker_right(const binary_tree_t *node, size_t *h)
+{
+	if (!node)
+		return;
+
+	(*h)++;
+	height_checker_right(node->right, h);
+}
+
 
 /**
  * binary_tree_height - Entry Point
@@ -40,14 +41,14 @@ size_t height_checker(const binary_tree_t *node, size_t *height)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_l = 0;
-	size_t height_r = 0;
+	size_t height_l = -1;
+	size_t height_r = -1;
 
 	if (!tree)
 		return (0);
 
-	height_l = height_checker(tree, &height_l);
-	height_r = height_checker(tree, &height_r);
+	height_checker_left(tree, &height_l);
+	height_checker_right(tree, &height_r);
 
 	return (height_l > height_r ? height_l : height_r);
 }
