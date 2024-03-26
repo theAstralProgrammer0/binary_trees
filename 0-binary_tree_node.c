25f7b6ea83898c5c35855924e4104ada81c18715
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-03-26 15:08:01 +0100

    Made corrections

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,7 +16,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode = NULL;
 
-	if ((newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t))) == NULL)
+	newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (!newnode)
 		return (NULL);
 
 	newnode->parent = parent;
