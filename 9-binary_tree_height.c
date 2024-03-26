23a6ea9c758c1e85af83165aca4d607fabfe98b1
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-03-26 16:16:11 +0100

    Testing correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -42,7 +42,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_l = 0;
 	size_t height_r = 0;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
 	height_checker(tree, &height_l);
