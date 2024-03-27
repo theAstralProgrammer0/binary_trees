1dacebfa70fdef31d1d6fbd7d5166f2d80991ae9
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-03-27 19:07:48 +0100

    Betty comlpiance

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,18 @@
 #include "binary_trees.h"
 
+/**
+ * is_tree_perfect - Auxilliary Function
+ *
+ * Description: tracks the leaf level and checks for perfection
+ *
+ * @tree: pointer to the root node of the tree to check
+ *
+ * @level: current level in the recusion.
+ *
+ * @leaf_level: current leaf node's level
+ *
+ * Return: (int) 1 if perfect, 0 otherwise
+ */
 int is_tree_perfect(const binary_tree_t *tree, int level, int *leaf_level)
 {
 	if (!tree)
@@ -19,6 +32,15 @@ int is_tree_perfect(const binary_tree_t *tree, int level, int *leaf_level)
 		is_tree_perfect(tree->right, level + 1, leaf_level));
 }
 
+/**
+ * binary_tree_is_perfect - Entry Point
+ *
+ * Description: checks if a binary tree is perfect
+ *
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: (int) 1 if perfect, 0 otherwise
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int leaf_level = -1;
