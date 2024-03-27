df5be34885560463c399e9e59ea6bb8802a432ab
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-03-27 19:22:28 +0100

    Debugging Debugger

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -52,7 +52,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaves = 0;
 
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
 	get_leaves(tree, &leaves);
