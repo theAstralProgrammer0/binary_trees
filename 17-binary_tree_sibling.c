246443cb2b5257edbf47c9ea26ed08068aa78383
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-03-27 19:14:27 +0100

    Double Betty compliance

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,5 +1,14 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_sibling - Entry Point
+ *
+ * Description: finds the sibling of a node
+ *
+ * @node: pointer to the node to find the sibling
+ *
+ * Return: (binary_tree_t *) pointer to the sibling node, NULL otherwise
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,14 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_uncle - Entry Point
+ *
+ * Description: finds the uncle of a node
+ *
+ * @node: pointer to the node to find the uncle
+ *
+ * Return: (binary_tree_t *) pointer to the uncle node, NULL otherwise
+ */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent)
