cff8d04e4813223fcb4e85ddbe13ce6b8e4aab05
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-03-27 09:36:04 +0100

    Found the depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,42 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * depth_checker - Auxilliary Funcntion
- *
- * Description: This is a function that recursively counts the tree levels
- *
- * @node: pointer to subject node
- *
- * @depth: pointer to depth of the tree
- *
- * Return: Nothing
- */
-void depth_checker(const binary_tree_t *node, size_t *depth, size_t *height)
-{
-	if (!node)
-		return;
-	if ((node->left || node->right) && (node->parent))
-	{
-		(*height)++;
-		(*depth
-	depth_checker(node->left, depth);
-	depth_checker(node->right, depth);
-}
-
-/**
  * binary_tree_depth - Entry Point
  *
- * Description: This is a function that measures the depth of a binary tree
+ * Description: measures the depth of a binary tree
  *
- * @tree: pointer to the root node of the tree to measure the depth.
+ * @tree: pointer to the root node of the tree to measure the depth
  *
- * Return: (size_t) depth of binary tree
+ * Return: (size_t) depth of the tree, 0 if tree is NULL
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t depth = 0;
-	size_t height = 0;
+	size_t depth_l = 0;
+	size_t depth_r = 0;
 
-	depth_checker(tree, &depth, &height);
-	return (depth);
+	if (!tree)
+		return (0);
+
+	if (tree->parent)
+		depth_l = 1 + binary_tree_depth(tree->parent);
+	else
+		depth_l = 0;
+	return (depth_l > depth_r ? depth_l : depth_r);
 }
