c2b43e0bc1c05746a14c8d330418f1b70a1c2baf
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-03-28 19:01:49 +0100

    Betty compliance

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,4 +1,13 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_rotate_right - Entry Point
+ *
+ * Description: a function that performs a right-rotation on a binary tree
+ *
+ * @tree: a pointer to the root node of the tree to rotate
+ *
+ * Return: pointer to the new root node of the tree once rotated
+ */
 
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
